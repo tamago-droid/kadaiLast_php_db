498eef7fb550fdeaf96459c3bb29a97bae8ae1e6
tamago-droid <80142175+tamago-droid@users.noreply.github.com>
2021-03-26 18:41:57 +0900

    ログイン機能追加

---
+++ login.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+
+<?php
+
+// セッション開始宣言
+// session_start();
+// 入力した値を受け取る
+$name = $_POST['name'];
+$pass = $_POST['pass'];
+// 中身確認
+// var_dump($pass);
+
+// データソースネーム
+$dsn = 'mysql:dbname=hha_db;charset=utf8;host=localhost';
+
+try {
+    $pdo = new PDO($dsn, "root", "root");
+} catch (PDOException $e) {
+        $msg = $e->getMessage();
+}
+
+// 入力したnameのデータを呼び出してる？
+$stmt = $pdo->prepare('SELECT * FROM user_table WHERE name = :name');
+$stmt->bindValue(':name', $name);
+$stmt->execute();
+$row = $stmt->fetch();
+//中身確認 
+// var_dump($row['password']);
+
+// tableのパスワードとマッチしているか
+if($pass == $row['password']) {
+    // セッションにnameを保存
+    // $_SESSION['name'] = $member['name'];
+
+    $msg = "ログインしました";
+    $link = "<a href=index.php>家計簿画面へ</a>"; 
+} else {
+    $msg = "name.かpass.が間違っています";
+    $link = "<a href=login_form.php>戻る</a>";
+}
+?>
+
+<h2><?= $msg?></h2>
+<div><?= $link?></div>
+</body>
+</html>
---
+++ login_form.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ログインページ</title>
+</head>
+<body>
+    <h1>ログインする</h1>
+    <form action="login.php" method="post">
+        <!-- 登録ネーム -->
+        <div>
+            <label for="name">name.</label>
+            <input type="text" name="name" id="name" required>
+        </div>
+
+        <!-- パスワード -->
+        <div>
+            <label for="pass">pass.</label>
+            <input type="text" name="pass" id="pass" required>
+        </div>
+
+        <div>
+            <input type="submit" value="Login">
+            <input type="reset" value="Reset">
+        </div>
+
+
+    </form>
+</body>
+</html>
